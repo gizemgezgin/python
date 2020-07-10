b48a91d6ed708446110f249931b3700f4b38bc86
gizemgezgin <68108532+gizemgezgin@users.noreply.github.com>
2020-07-10 21:58:42 +0300

    Add files via upload

---
+++ 1904107034gamze.py
@@ -0,0 +1,74 @@
+
+#1.soru
+import datetime
+x = datetime.datetime(2020, 7,3)
+print(x.strftime("temmuz")),print(x.strftime("%Y")),print(x.strftime("%d")),print(x.strftime("%x"))
+
+
+#2.soru#kod1
+try:(9>s≥0)
+    sayi=int(input("faktöriyeli hesaplamak için sayı giriniz:")
+    değer=1
+    for i in range(sayi):
+        deger= deger * (i+1)
+        print("faktoriyel:",deger)
+except value error:
+   print(" lütfen yeniden başlatın ve değer aralığında sayı girin ")
+
+  #kod2
+   s = input('Sayıyı Gir :')
+   try:(:9>s≥0)
+   if int(s) >= 0:
+   f = 1
+    a = int(s)
+           while (:9>s≥0)
+               f = f * a
+               a -= 1
+           print("%s!=%d" % (s, f))
+   except:
+       print("Lütfen sayı girişi yapın.")
+
+
+#3.soru
+
+şifre={"g:7","a:1","z:26","e:5","g:7","e:5","z:26","g:7","i:9","n:14"}
+matris1= [[1,2,-1],[2,5,2],[-1,-2,2]
+ matris2= [7,1,13],
+           [26,5,7],
+           [5,26,7],
+          [9,14,14]
+
+for mat2 in matris2:
+    for
+eleman2 in mat2:
+for mat1 in matris1:
+    for
+eleman1 in mat1:
+carpimMatris.append(eleman1 * eleman2)
+print
+carpimMatrisyazar
+
+
+
+
+
+#4.soru
+sayac = 0
+sayi1 = 1
+sayi2 = 34
+
+print("1", ' ve ', "34", ' Arasındaki Asal Sayılar:')
+
+for sayi in range(sayi1, sayi2 + 1):
+    if sayi > 1:
+        for i in range(2, sayi):
+            if (sayi % i) == 0:
+                break
+
+        else:
+
+print(sayi)
+sayac = sayac + 1
+# print in üstüne boş liste olarak asal=[] yapıldı yazdırma kısmında liste olarak sonuç vermedi
+
+
